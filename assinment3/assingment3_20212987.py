03c100ca0ab82cdca865958b6335b2d68bc7f9c3
munjihwan020627 <85243418+munjihwan020627@users.noreply.github.com>
2021-09-30 19:19:28 +0900

    Update assingment3_20212987.py

--- assinment3/assingment3_20212987.py
+++ assinment3/assingment3_20212987.py
@@ -33,58 +33,54 @@ def doScoreDB(scdb):
         if inputstr == "": continue
         parse = inputstr.split(" ")
         if parse[0] == 'add':
-            try:
-                record = {'Name':parse[1], 'Age':parse[2], 'Score':parse[3]}
+
+            if type(parse[2]) == int or type(parse[3]) == int:
+                record = {'Name': parse[1], 'Age': parse[2], 'Score': parse[3]}
                 scdb += [record]
-            except IndexError:
-                print("추가할 학생의 이름, 나이, 점수를 공백을 두고 입력하세요.")
+            else:
+                print('다시입력해주세요')
         elif parse[0] == 'del':
             try:
                 for p in scdb:
                     if p['Name'] == parse[1]:
                         scdb.remove(p)
+                        break
             except IndexError:
-                print('삭제할 학생의 이름을 입력하세요.')
+                print("index error")
         elif parse[0] == 'show':
-            try:
-                sortKey ='Name' if len(parse) == 1 else parse[1]
-                showScoreDB(scdb, sortKey, 'All')
-            except KeyError:
-              print('show 뒤에는 정렬 기준을 입력하세요.')
+            sortKey ='Name' if len(parse) == 1 else parse[1]
+            showScoreDB(scdb, sortKey)
         elif parse[0] == 'find':
-            try:
-                sortKey = 'Score'
-                showScoreDB(scdb, sortKey, parse[1])
-            except IndexError:
-                print('find 뒤에는 찾고자 하는 학생의 이름을 입력하세요.')
+            lst =[]
+            for a in scdb:
+                if a['Name'] == parse[1]:
+                    lst.append(a)
+            showScoreDB(lst, 'Name')
         elif parse[0] == 'inc':
             try:
-                addScore(scdb, parse[1], parse[2])
-            except IndexError:
-                print('inc 뒤에는 학생의 이름과 추가할 점수를 공백을 두고 입력하세요.')
+                for a in scdb:
+                    if a['Name'] == parse[1]:
+                        a['Score'] = str(int(parse[2]) + int(a['Score']))
             except ValueError:
-                print('추가할 점수는 정수 형태로 입력해주세요.')
+                print("Value error")
+        elif parse[0] == 'del':
+            for a in scdb:
+                if 'name' == parse[1]:
+                    scdb.remove(a)
         elif parse[0] == 'quit':
             break
         else:
             print("Invalid command: " + parse[0])
 
 
-def showScoreDB(scdb, keyname, findName):
+def showScoreDB(scdb, keyname):
     for p in sorted(scdb, key=lambda person: person[keyname]):
         for attr in sorted(p):
-            if (findName == 'All') or (p['Name'] == findName):
-                print(attr + "=" + p[attr], end=' ')
-        if (findName == 'All') or (p['Name'] == findName):
-            print()
-
-def addScore(scdb, Name, amount):
-    for p in scdb:
-      if Name == p['Name']:
-        p['Score'] = int(p['Score']) + int(amount)
-        p['Score'] = str(p['Score'])
+            print(attr + "=" + p[attr], end=' ')
+        print()
 
 
 scoredb = readScoreDB()
 doScoreDB(scoredb)
 writeScoreDB(scoredb)
+
